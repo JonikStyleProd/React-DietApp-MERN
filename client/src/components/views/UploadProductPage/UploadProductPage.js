cebc0076ac45c5d42c06536bfbc256e4bea118cd
tydiet <tydiet1@gmail.com>
2020-08-31 19:16:02 +0300

    Starting UploadProductPage

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UploadProductPage() {
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
+            <div style={{textAlign: 'center', marginBottom: '2rem' }}>
+                <h1>Upload Food</h1>
+                <h5>Upload what you ate today!!!</h5>
+            </div>
+        </div>
+    )
+}
+
+export default UploadProductPage;
